41270c10fc059a7b88ad55c0eb4ba6ddc14d7137
Thông Phạm Duy <21522645@gm.uit.edu.vn>
2025-01-16 23:10:25 +0700

    add more data to question game

--- src/main/java/com/example/demo/services/ListeningQuestionService.java
+++ src/main/java/com/example/demo/services/ListeningQuestionService.java
@@ -25,6 +25,10 @@ public class ListeningQuestionService {
 		if (listeningQuestionRepository.findAll().size() == 0) {
 			listeningQuestionRepository.save(new ListeningQuestion(1, "https://www.youtube.com/watch?v=f7KX1AwgZ3w", "What is the main content of the video ?", "A girl is talking about her interests", "A girl is teaching how to play tennis", "A girl would like to go to cinema with her friend", "None of them", "A"));
 			listeningQuestionRepository.save(new ListeningQuestion(2, "https://www.youtube.com/watch?v=ry9SYnV3svc", "What is the working of the man in the video ?", "08:30 - 17:30", "09:00 - 18:00", "Another working time", "The video does not provide this information", "C"));
+			listeningQuestionRepository.save(new ListeningQuestion(3, "https://www.youtube.com/watch?v=6mo3jc0WILI", "What is the main purpose of setting goal ?", "To keep you busy", "To focus your efforts and give direction", "To make you popular", "To confuse you", "B"));
+			listeningQuestionRepository.save(new ListeningQuestion(4, "https://www.youtube.com/watch?v=TTn0J7MT5lU&t=4s", "What about the coffee in New Zealand ?", "The coffee very bitter", "It is usually hot coffee", "The coffee is usually mixed with milk", "The coffee is very good", "D"));
+			listeningQuestionRepository.save(new ListeningQuestion(5, "https://www.youtube.com/watch?v=kePBvNotYy4", "What time does she go to bed ?", "10:00 AM", "10:00 PM", "This information does not appear in the video", "All of answers are wrong", "B"));
+
 		}
 	}
 }
